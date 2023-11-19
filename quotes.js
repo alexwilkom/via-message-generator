28197314222f09f36563efa165a2b3299af43156
Alex Wilkom <alex.wilkom@icloud.com>
2023-11-19 11:09:21 +0100

    initial commit: quote random generator

---
+++ quotes.js
@@ -0,0 +1,21 @@
+const quotes = [
+    "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+    "Some people look for a beautiful place. Others make a place beautiful.",
+    "Happiness is not by chance, but by choice.",
+    "If I cannot do great things, I can do small things in a great way.",
+    "The bad news is time flies. The good news is you're the pilot.",
+    "You make a life out of what you have, not what you're missing.",
+    "It's how you deal with failure that determines how you achieve success."
+]
+
+function generateRandomNumber(num) {
+    return Math.floor(Math.random() * num);
+}
+
+function outputQuote(quotes) {
+    const indexNumber = generateRandomNumber(quotes.length);
+    const quote = quotes[indexNumber];
+    console.log(quote);
+}
+
+outputQuote(quotes);
